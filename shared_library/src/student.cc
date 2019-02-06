ea29be90977ed65abd8e61c79d5447852576f81d
Julian Domingo <jddoming@amazon.com>
2019-02-06 14:22:03 -0600

    Added shared + static library cmake examples

---
+++ shared_library/src/student.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <student.h>
+
+int test1(void)
+{
+    std::cout << "Test1" << std::endl;
+    return 1;
+}
+
+void test2(void)
+{
+    std::cout << "Test2" << std::endl;
+}
